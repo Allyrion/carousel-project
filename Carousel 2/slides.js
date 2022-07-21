f8ef166abca220807963cbc5e896cc1c55fbd7fc
Allyrion <kishan.solomon@gmail.com>
2022-07-21 17:11:48 -0400

    Carousel 2 is mobile compatible

--- Carousel 2/slides.js
+++ Carousel 2/slides.js
@@ -6,52 +6,90 @@ const prevButton = document.querySelector (".prev");
 const slides = document.getElementsByClassName("carouselTwo-slides");
 const anchor = document.querySelector("#anchor");
 
-//let viewportWidth = window.innerWidth;
-let viewportWidth;
 
-//slider count
-let slidePos = 1;
+// Create a condition that targets viewports at least 768px wide
+const mediaQuery = window.matchMedia('(min-width: 768px)')
 
-const getWidth = () => {
-    viewportWidth = window.innerWidth;
+let mobileScreen = true;
+
+function handleTabletChange(e) {
+  // Check if the media query is true
+  if (e.matches) {
+    // Then log the following message to the console
+    mobileScreen = false;
+  }
 }
+// Register event listener
+mediaQuery.addListener(handleTabletChange)
+
+// Initial check
+handleTabletChange(mediaQuery)
 
 let marginLeft = 0;
-let speed = 50;
-let maxMargin = 1800;
+let speed;
+let maxMargin;
+
+//Settings for desktop
+if (mobileScreen == false){
+    speed = 50;
+    maxMargin = 1800;
+} else if (mobileScreen == true) {
+    speed = 25;
+    maxMargin = 2100;
+}
+
+//Next Function
+const nextMovement = () => {
+    if (marginLeft < maxMargin) {
+        const carouselNext = setInterval(()=>{
+            anchor.style.marginLeft = `-${marginLeft+speed}px`;
+            marginLeft = marginLeft + speed;
+            console.log(marginLeft);
+        }, 25)
+
+        const stopNext = setTimeout(() => {
+            clearInterval(carouselNext);
+        }, 350)
+    }
+}
+
+//Previous Function
+const prevMovement = () => {
+    if (marginLeft > 0) {
+        const carouselNext = setInterval(()=>{
+            anchor.style.marginLeft = `-${marginLeft-speed}px`;
+            marginLeft = marginLeft - speed;
+            console.log(marginLeft);
+        }, 25)
+
+        const stopNext = setTimeout(() => {
+            clearInterval(carouselNext);
+        }, 350)
+    }
+}
+
 
 nextButton.addEventListener("click", ()=> {
 
-        
-        if (marginLeft < maxMargin) {
-            const carouselNext = setInterval(()=>{
-                anchor.style.marginLeft = `-${marginLeft+speed}px`;
-                marginLeft = marginLeft + speed;
-                console.log(marginLeft);
-            }, 25)
-
-            const stopNext = setTimeout(() => {
-                clearInterval(carouselNext);
-            }, 350)
-        }
+        nextMovement();
+        console.log(mobileScreen);
 
 
 });
 
 prevButton.addEventListener("click", ()=> {
 
-
-        if (marginLeft > 0) {
-            const carouselNext = setInterval(()=>{
-                anchor.style.marginLeft = `-${marginLeft-speed}px`;
-                marginLeft = marginLeft - speed;
-                console.log(marginLeft);
-            }, 25)
-
-            const stopNext = setTimeout(() => {
-                clearInterval(carouselNext);
-            }, 350)
-        }
+    prevMovement();
+        
+});
 
 
+
+$(document).on("pagecreate","#page_one",function(){
+    $("#carouselTwo").on("swipeleft",function(){
+        nextMovement();
+    });
+    $("#carouselTwo").on("swiperight",function(){
+        prevMovement();
+    });
 });
