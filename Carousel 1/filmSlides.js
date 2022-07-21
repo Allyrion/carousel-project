13c566f2b3b7be69968ff6a5352afa88ca5607d8
Allyrion <kishan.solomon@gmail.com>
2022-07-20 21:36:06 -0400

    Added bottom lines for navigation

--- Carousel 1/filmSlides.js
+++ Carousel 1/filmSlides.js
@@ -5,23 +5,29 @@ const filmTwoLinks = document.querySelectorAll(".film2-link");
 const imageGrid = document.querySelector(".image-grid");
 const film1_img = document.querySelector("#filmOneImg");
 const film2_img = document.querySelector("#filmTwoImg");
+const lines = document.querySelectorAll(".line")
 
 
 
 const filmTwoFocus = () => {
     imageGrid.style.marginLeft = "-45%";
     film1_img.style.height = "500px";
-    film2_img.style.height = "600px"
+    film2_img.style.height = "600px";
+    lines[1].classList.add("active")
+    lines[0].classList.remove("active")
 }
 
 const filmOneFocus = () => {
     imageGrid.style.marginLeft = "20%";
     film1_img.style.height = "600px";
-    film2_img.style.height = "500px"
+    film2_img.style.height = "500px";
+    lines[0].classList.add("active")
+    lines[1].classList.remove("active")
 }
 
 filmOneLinks[0].addEventListener("click", ()=> {
     filmOneFocus();
+    
 });
 
 filmTwoLinks[0].addEventListener("click", ()=> {
@@ -36,3 +42,10 @@ filmTwoLinks[1].addEventListener("click", ()=> {
     filmTwoFocus();
 });
 
+filmOneLinks[2].addEventListener("click", ()=> {
+    filmOneFocus();
+});
+
+filmTwoLinks[2].addEventListener("click", ()=> {
+    filmTwoFocus();
+});
