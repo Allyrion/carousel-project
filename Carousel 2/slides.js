ad00b9cf62056955168f14ae61869348388b36dd
Allyrion <kishan.solomon@gmail.com>
2022-07-21 00:30:55 -0400

    Added set interval to slide, next mostly works but previous needs work

--- Carousel 2/slides.js
+++ Carousel 2/slides.js
@@ -34,15 +34,29 @@ nextButton.addEventListener("click", ()=> {
 
             slides[slidesHidden].classList.add("hideSlide");
             slides[slidesHidden].classList.remove("animate__slideInRight");
-            slides[slidesHidden+1].classList.add("hideSlide");
-            slides[slidesHidden+1].classList.remove("hideSlide");
             slidesHidden += 1;
 
         }
 
         slides[slidesHidden].classList.add("animate__animated", "animate__slideInRight");
         slides[slidesHidden+1].classList.add("animate__animated", "animate__slideInRight");
-        slides[slidesHidden+2].classList.add("animate__animated", "animate__slideInRight");
+
+        if (slides[slidesHidden+2] !== undefined) {
+            slides[slidesHidden+2].classList.add("animate__animated", "animate__slideInRight");
+            slides[slidesHidden+2].classList.add("hideSlide");
+        }
+
+        slides[slidesHidden].classList.add("hideSlide");
+        slides[slidesHidden+1].classList.add("hideSlide");
+  
+
+        const resetDisplayNext = setTimeout(() => {
+            slides[slidesHidden].classList.remove("hideSlide");
+            slides[slidesHidden+1].classList.remove("hideSlide");
+            if (slides[slidesHidden+2] !== undefined) {
+                slides[slidesHidden+2].classList.remove("hideSlide");
+            }
+        }, 1);
 
     }
 
@@ -53,15 +67,21 @@ prevButton.addEventListener("click", ()=> {
         for (let i = 0; i < 2; i++) {
             
             slides[slidesHidden].classList.add("animate__slideInLeft");
-            if (slides[slidesHidden-1] !== undefined){
-                slides[slidesHidden-1].classList.remove("animate__slideInLeft");
+            if (slides[slidesHidden+2] !== undefined){
+                slides[slidesHidden+2].classList.remove("animate__slideInLeft");
             }
             
             slides[slidesHidden].classList.remove("hideSlide");
+
+            /* const resetDisplayPrev = setTimeout(() => {
+                slides[slidesHidden].classList.add("animate__slideInLeft");
+                slides[slidesHidden].classList.remove("hideSlide");
+            }, 1); */
             
             if (slidesHidden !== 0) {
                 slidesHidden -= 1;
             }
+
         }
 
 
